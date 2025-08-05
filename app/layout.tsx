2d376461a677e33eadd25d331451d5b689a5c4be
Remus36 <blueremus@yahoo.com>
2025-08-05 17:30:35 +0300

    Fix: Re-linked globals.css in layout and cleaned up CSS file

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   // Aici vei pune titlul ales de tine!
-  title: "PixelForge 3D - De la concept la realitate",
+  title: "Creative 3D - De la concept la realitate",
   description: "Portofoliu de modelare 3D si magazin de produse printate.",
 };
 
